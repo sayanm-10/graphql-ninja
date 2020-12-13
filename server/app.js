b214de53c8e2fce35c4647fbd2a2e5d3d704791c
sayanm-10 <sayan.mukherjee108@gmail.com>
2020-12-13 17:06:18 -0500

    secret management

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ const schema = require('./schema/schema');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 
+const SECRETS = require('./config/secrets.json');
+const DB_NAME = 'graphql-test';
+
 const app = express();
 
 // DB connection
-mongoose.connect('mongodb+srv://graphql-admin:ninja@cluster0.j8x9m.mongodb.net/graphql-test?retryWrites=true&w=majority');
+mongoose.connect(`mongodb+srv://${SECRETS.DB_UNAME}:${SECRETS.DB_PWD}@cluster0.j8x9m.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
 mongoose.connection.once('open', () => {
     console.log('CONNECTED TO DB...');
 });
