a46656119285f1f0f0b8d6d75307d5823ba72917
sayanm-10 <sayan.mukherjee108@gmail.com>
2020-12-13 17:01:13 -0500

    add Mongo connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const schema = require('./schema/schema');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB connection
+mongoose.connect('mongodb+srv://graphql-admin:ninja@cluster0.j8x9m.mongodb.net/graphql-test?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('CONNECTED TO DB...');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
